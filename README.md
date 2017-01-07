7090ca132739623595226a896a75b2b5a2a60341
Alex Nichol <unixpickle@gmail.com>
2017-01-07 12:31:58 -0500

    parse sensor output

---
+++ README.md
@@ -0,0 +1,3 @@
+# sensorlog
+
+This is a very simple tool to log the output of `senors` (a Linux tool) to a CSV file.
---
+++ sensors.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+func ReadSensors() (map[string]string, error) {
+	cmd := exec.Command("sensors")
+	var output bytes.Buffer
+	cmd.Stdout = &output
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+	return parseOutput(output.String()), nil
+}
+
+func parseOutput(out string) map[string]string {
+	res := map[string]string{}
+	lines := strings.Split(out, "\n")
+	fieldExpr := regexp.MustCompile(`^(.*?):\s*(.*?)(\(|$)`)
+	for _, line := range lines {
+		match := fieldExpr.FindStringSubmatch(line)
+		if match == nil {
+			continue
+		}
+		if match[1] == "Adapter" {
+			continue
+		}
+		res[match[1]] = strings.TrimSpace(match[2])
+	}
+	return res
+}
